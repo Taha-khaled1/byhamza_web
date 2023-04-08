309816bf1a6236f89ba563d969a54e1b667196ef
anaseem80 <anaseemamin@gmail.com>
2023-04-09 00:17:47 +0200

    changes

--- resources/views/site/homePage/homePage.blade.php
+++ resources/views/site/homePage/homePage.blade.php
@@ -202,7 +202,7 @@
                  <div class="row">
                     @foreach ($images as $item)
                     <div class="d-flex flex-column item col-lg-3 col-6 overflow-hidden mb-3">
-                        <img src="{{asset($item->image)}}" alt="" style="height: 320px">
+                        <img src="{{asset($item->image)}}" alt="" class="custom-imgg">
                     </div>
                     @endforeach
                 </div>
