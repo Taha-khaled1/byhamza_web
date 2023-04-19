8c3a61e2818c19ffdf8bb5ea89c008377419547c
Taha khalid <tth31770@gmail.com>
2023-04-19 06:59:21 +0200

    first commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -69,6 +69,5 @@ class Kernel extends HttpKernel
         'localeSessionRedirect'   => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
         'localeCookieRedirect'    => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
         'localeViewPath'          => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
-
     ];
 }
